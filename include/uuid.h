f4246d218c92005bedb6b7cac52611d171d5bdb1
Mauro Deryckere <mauro.deryckere@gmail.com>
2025-06-21 23:05:22 +0200

    [Setup] tests framework setup

---
+++ include/uuid.h
@@ -0,0 +1,9 @@
+#ifndef MAU_UUID
+#define MAU_UUID
+
+namespace MauUUID
+{
+	int constexpr test{ 10 };
+}
+
+#endif
---
+++ tests/test_uuid.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+#include "uuid.h"
+
+TEST_CASE("Dummy test", "[uuid]")
+{
+    REQUIRE(true);
+}
